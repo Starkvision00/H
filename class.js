caab4c74b3be15f33dd0064cee1f4d24cf3d4557
Starkvision00 <mrstark0103@gmail.com>
2024-10-15 10:32:22 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,11 @@
+document.getElementById('reveal').addEventListener('click', function() {
+    document.getElementById('proposal').classList.toggle('hidden');
+});
+
+document.getElementById('accept').addEventListener('click', function() {
+    alert('I Loveeeee Youuuuu ❤️');
+});
+
+document.getElementById('decline').addEventListener('click', function() {
+    alert('Nee No click pannalum naa solla dhan seiven,, Loveeee youuu Diii Azhagiiii ❤️😊');
+}); 
